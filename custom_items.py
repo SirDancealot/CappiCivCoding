21cfe7b95a211d3309a11fc5708cf842cb2c9f63
Sir Eraisuithon <mj@lpj-software.dk>
2023-06-28 14:04:53 +0200

    Added book_voucher custom item

--- custom_items.py
+++ custom_items.py
@@ -6,3 +6,4 @@ CUSTOM_ITEMS["p_key"] = "{{id: \"clock\", tag: {{CustomModelData: 2670003, displ
 CUSTOM_ITEMS["d_key"] = "{{id: \"clock\", tag: {{CustomModelData: 2670004, display: {{Name: '{{\"text\":\"Blood-Stained Key\", \"italic\":false}}'}}}}, Count: {0}}}"
 CUSTOM_ITEMS["p_pile"] = "{{id: \"clock\", tag: {{CustomModelData: 2670005, display: {{Name: '{{\"text\":\"Pile of Potatoes\", \"italic\":false}}'}}}}, Count: {0}}}"
 CUSTOM_ITEMS["p_barrel"] = "{{id: \"clock\", tag: {{CustomModelData: 2670006, display: {{Name: '{{\"text\":\"Barrel of Potatoes\", \"italic\":false}}'}}}}, Count: {0}}}"
+CUSTOM_ITEMS["book_v"] = "{{id: \"clock\", tag: {{CustomModelData: 2670007, display: {{Name: '{{\"text\":\"Book Token\", \"italic\":false}}'}}}}, Count: {0}}}"
