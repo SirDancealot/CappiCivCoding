c867262352c58c09aecb656c5550ad502af64848
Sir Eraisuithon <mj@lpj-software.dk>
2023-07-02 02:34:20 +0200

    Improved yes no detection for dual trades

--- create_villager.py
+++ create_villager.py
@@ -1,5 +1,4 @@
-from custom_items import CUSTOM_ITEMS
-from generator_methods import generate_item
+from generator_methods import generate_item, YES_OPTIONS
 
 horn = "{{id: \"goat_horn\", tag: {{instrument: {0}_goat_horn}}, Count: 1}}"
 item = "{{id:\"{0}\",Count:{1}}}"
@@ -17,16 +16,16 @@ trade = "{{buy:{0},sell:{1},rewardExp:0b,maxUses:9999999}}"
 dual_trade = "{{buy:{0},buyB:{1},sell:{2},rewardExp:0b,maxUses:9999999}}"
 
 for i in range(num_trades):
-    do_dual_trade = input("dual-trade?: [y/n]").lower()
+    do_dual_trade = input("dual-trade?: [y/N]").lower()
 
-    if do_dual_trade == "y":
+    if do_dual_trade in YES_OPTIONS:
         item_1 = generate_item("generating buy item 1", False)
         item_2 = generate_item("generating buy item 2", False)
         sell_item = generate_item("generating sell item", False)
         trade_list.append(dual_trade.format(item_1, item_2, sell_item))
     else:
-        buy_item = generate_item("generating buy item", False)
-        sell_item = generate_item("generating sell item", False)
+        buy_item = generate_item("generating buy item")
+        sell_item = generate_item("generating sell item")
         trade_list.append(trade.format(buy_item, sell_item))
 
 trade_string = ",".join(trade_list)
